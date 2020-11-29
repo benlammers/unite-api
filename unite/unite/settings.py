1498af07047ee8e41e728aeafb215220aa79bb68
benlammers <21benlammers@gmail.com>
2020-11-28 21:34:17 -0500

    Add Heroku Config

--- unite/unite/settings.py
+++ unite/unite/settings.py
@@ -175,3 +175,5 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
